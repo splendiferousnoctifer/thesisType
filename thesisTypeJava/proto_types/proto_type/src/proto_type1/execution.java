8b36a215491980a2a44474c8ec195660157df7b5
splendiferousnoctifer <63741955+splendiferousnoctifer@users.noreply.github.com>
2021-01-19 14:01:20 +0100

    update thesisTypeJava
    added java files

---
+++ thesisTypeJava/proto_types/proto_type/src/proto_type1/execution.java
@@ -0,0 +1,9 @@
+package proto_type1;
+public class execution {
+
+	public static void main(String[] args) {
+//		new determineKey();
+		new keyboard();
+	}
+
+}
---
+++ thesisTypeJava/proto_types/proto_type/src/proto_type2/execution.java
@@ -0,0 +1,8 @@
+package proto_type2;
+public class execution {
+
+	public static void main(String[] args) {
+		new keylog();
+	}
+
+}
